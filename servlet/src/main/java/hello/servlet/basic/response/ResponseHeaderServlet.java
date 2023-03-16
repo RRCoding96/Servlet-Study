74707a21294e245f66e3eb425d1ad827c3f3c5c2
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-03-16 22:06:36 +0900

    feat: ResponseHeaderServlet redirect 편의 메서드

--- servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -26,7 +26,7 @@ public class ResponseHeaderServlet extends HttpServlet {
         //[Header 편의 메서드]
         content(response);
         cookie(response);
-//        redirect(response);
+        redirect(response);
         //[message body]
         PrintWriter writer = response.getWriter();
         writer.println("ok");
@@ -49,4 +49,12 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.addCookie(cookie);
     }
 
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        // response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+    }
 }
